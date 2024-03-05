66bc64adbabe67528500ab401d3bd32da0eb87fa
Juan Ignacio Grodz <jigrodz@gmail.com>
2024-03-05 12:34:09 -0300

    Seguimos con modificaciones esteticas

--- script.js
+++ script.js
@@ -1,79 +1,61 @@
 const textArea = document.querySelector('.text-area');
-const mensaje = document.querySelector('.mensaje');
+let vistaSinMensaje = document.getElementById("sinMensaje");
+let mensajeEncriptado = document.getElementById("msj-encriptado");
+let revelarTexto = document.getElementById("texto-revelado");
 
-// La letra "e" es convertida para "enter"
-// La letra "i" es convertida para "imes"
-// La letra "a" es convertida para "ai"
-// La letra "o" es convertida para "ober"
-// La letra "u" es convertida para "ufat"
-function btnEncriptar(){
-  const textoEncriptado = encriptar(textArea.value)
-  mensaje.value = textoEncriptado
-  textArea.value = "";
-  mensaje.style.backgroundImage = 'none'
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    revelarTexto.textContent = textoEncriptado; // Mostrar texto encriptado en el elemento correspondiente
+    textArea.value = ""; // Limpiar área de texto
+    cambiarAside(); // Cambiar a la sección con el texto encriptado
 }
-
 
 function encriptar(stringEncriptado) {
     let matrizCodigo = [["e", 'enter'], ["i", 'imes'], ["a", 'ai'], ["o", 'ober'], ["u", 'ufat']];
     stringEncriptado = stringEncriptado.toLowerCase()
 
     for (let i = 0; i < matrizCodigo.length; i++) {
-if(stringEncriptado.includes(matrizCodigo[i][0])){
-    stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
-
-}
+        if (stringEncriptado.includes(matrizCodigo[i][0])) {
+            stringEncriptado = stringEncriptado.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
     }
 
-return stringEncriptado
+    return stringEncriptado;
 }
 
-
-
-
-
-
-
-function btnDesencriptar(){
-    const textoEncriptado = desencriptar(textArea.value)
-    mensaje.value = textoEncriptado
-    textArea.value = "";
-    
-  }
-
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(textArea.value);
+    revelarTexto.textContent = textoEncriptado; // Mostrar texto desencriptado en el elemento correspondiente
+    textArea.value = ""; // Limpiar área de texto
+    cambiarAside(); // Cambiar a la sección con el texto encriptado
+}
 
 function desencriptar(stringDesencriptado) {
     let matrizCodigo = [["e", 'enter'], ["i", 'imes'], ["a", 'ai'], ["o", 'ober'], ["u", 'ufat']];
     stringDesencriptado = stringDesencriptado.toLowerCase()
 
     for (let i = 0; i < matrizCodigo.length; i++) {
-if(stringDesencriptado.includes(matrizCodigo[i][1])){
-    stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
-
-}
+        if (stringDesencriptado.includes(matrizCodigo[i][1])) {
+            stringDesencriptado = stringDesencriptado.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
     }
 
-return stringDesencriptado
+    return stringDesencriptado;
 }
 
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const botonCopiar = document.querySelector('.copiar');
-    const textarea = document.querySelector('.mensaje');
-
-    botonCopiar.addEventListener('click', function() {
-        textarea.select();
-        document.execCommand('copy');
-       
-    });
-});
-
-
-function mostrarBotonCopiar() {
-    const botonCopiar = document.querySelector('.copiar');
-    botonCopiar.style.display = 'absolute'
-    botonCopiar.style.marginTop = '505px'; // Ajustar margen superior según sea necesario
-    botonCopiar.style.marginLeft = '125px'; // Ajustar margen izquierdo según sea necesario
+function cambiarAside() {
+    vistaSinMensaje.style.display = "none";
+    mensajeEncriptado.style.display = "block";
 }
+
+const copiarMsjEncriptado = async () => {
+    //.textContent para obtener el contenido de texto dentro de ese elemento
+    let textoEncriptado = document.getElementById("texto-revelado").innerHTML;
+    try {
+      // Intentar copiar el texto al portapapeles utilizando la API del portapapeles
+      await navigator.clipboard.writeText(textoEncriptado);
+      console.log("Contenido copiado al portapapeles");
+    } catch (error) {
+      console.error("Error al copiar: ", error);
+    }
+  };
